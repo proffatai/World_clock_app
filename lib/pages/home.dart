54b2c9151c9e80003ead2e22350c66428edbe686
proffatai <proffatai@gmail.com>
2021-08-26 14:02:24 +0100

    FlatButton has been updated to TextButton

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,14 +6,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
 
   @override
   Widget build(BuildContext context) {
-
     data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
-    
 
     //set background
     String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
@@ -24,39 +21,38 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/$bgImage'),
-              fit: BoxFit.cover
-            )
-          ),
+              image: DecorationImage(
+                  image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
           child: Padding(
             padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
             child: Column(
               children: <Widget>[
-                FlatButton.icon(
-                  onPressed: () async {
-                    dynamic result = await Navigator.pushNamed(context, '/location');
-                    setState(() {
-                      data = {
-                        'time': result['time'],
-                        'location': result['location'],
-                        'isDaytime': result['isDaytime'],
-                        'flag': result['flag']
-                      };
-                    });
-                  },
-                  icon: Icon(
-                    Icons.edit_location,
-                    color: Colors.grey[300],
-                  ),
-                  label: Text(
-                    'Edit Location',
-                    style: TextStyle(
+                TextButton.icon(
+                    onPressed: () async {
+                      dynamic result =
+                          await Navigator.pushNamed(context, '/location');
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'isDaytime': result['isDaytime'],
+                          'flag': result['flag']
+                        };
+                      });
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
                       color: Colors.grey[300],
                     ),
-                  )
+                    label: Text(
+                      'Edit Location',
+                      style: TextStyle(
+                        color: Colors.grey[300],
+                      ),
+                    )),
+                SizedBox(
+                  height: 20.0,
                 ),
-                SizedBox(height: 20.0,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -70,7 +66,9 @@ class _HomeState extends State<Home> {
                     )
                   ],
                 ),
-                SizedBox(height: 20.0,),
+                SizedBox(
+                  height: 20.0,
+                ),
                 Text(
                   data['time'],
                   style: TextStyle(
